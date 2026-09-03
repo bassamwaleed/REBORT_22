b7aa95e8ff3dae7cbd4b613b94f1f22905e08407
bassamwaleed <125300520+bassamwaleed@users.noreply.github.com>
2026-09-03 20:32:51 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// تأكد من وجود ملف index.css في نفس المجلد (src)
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
